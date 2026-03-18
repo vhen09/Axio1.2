8bf94feba51e41ca11cd4491dcd9f37837151b90
vhen09 <vhenkarasu@gmail.com>
2026-03-19 05:00:16 +0800

    Fix double class declaration error - use database.php instead of database-fixed.php in auto-setup

--- backend/config/auto-setup.php
+++ backend/config/auto-setup.php
@@ -16,7 +16,7 @@ if ($lockAge < 3600) {
 @touch($setupLockFile);
 
 try {
-require_once __DIR__ . '/../config/database-fixed.php';
+    require_once __DIR__ . '/database.php';
     $db = new Database();
     $pdo = $db->getConnection();
     
